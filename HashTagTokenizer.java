6b0f64ec101e1bde87ae08343fa40f12fa18dae5
Yuli peron <yulilidya.peron@post.runi.ac.il>
2024-01-31 19:49:11 +0200

    try

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -11,16 +11,28 @@ public class HashTagTokenizer {
 
 	public static String[] readDictionary(String fileName) {
 		String[] dictionary = new String[3000];
-
 		In in = new In(fileName);
-
 		// Your code here
+
+		for (int i = 0; i<dictionary.length; i++)
+		{
+			dictionary[i] = in.readLine();
+		}
 
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
 		// Your code here
+		for(int i = 0; i<dictionary.length; i++)
+		{
+			if(dictionary[i].equals(word))
+			{
+				return true;
+			}
+
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
@@ -31,10 +43,18 @@ public class HashTagTokenizer {
         }
  
         int N = hashtag.length();
+		hashtag = hashtag.toLowerCase();
 
-        for (int i = 1; i <= N; i++) {
-		
-        }
+        for (int i = 1; i <= N; i++) 
+		{
+			String part = hashtag.substring(0, i);
+			if(existInDictionary(part, dictionary))
+			{
+				System.out.println(part);
+				breakHashTag(hashtag.substring(i), dictionary);
+				return;
+			}
+        }	
     }
 
 }
